e3809df300ccf9c99a7846060c0072eb6f584221
peironggg <wupeirong294@gmail.com>
2018-04-01 21:19:14 +0800

    Set up Question/Welcome Screen/Question VC

---
+++ RTT/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RTT
+//
+//  Created by Wu Peirong on 1/4/18.
+//  Copyright © 2018 Wu Peirong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var beginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        beginButton.layer.cornerRadius = 10
+        
+    }
+
+   
+
+}
+
---
+++ RTT/Model/Question.swift
@@ -0,0 +1,22 @@
+//
+//  Question.swift
+//  RTT
+//
+//  Created by Wu Peirong on 1/4/18.
+//  Copyright © 2018 Wu Peirong. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    var questionTitle: String = ""
+    var questionAnswer: Int = 0
+    
+    init(title: String, answer: Int) {
+        
+        questionTitle = title
+        questionAnswer = answer
+    }
+    
+}
