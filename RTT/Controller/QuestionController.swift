61c1a9507b6a212e674cf20a6f13a654bc03864b
peironggg <wupeirong294@gmail.com>
2018-04-05 08:52:14 +0800

    Put in button action functionality

--- RTT/Controller/QuestionController.swift
+++ RTT/Controller/QuestionController.swift
@@ -12,6 +12,7 @@ class QuestionController: UIViewController {
     
     var questionNumber: Int = 0
     var score: Int = 0
+    var chosenAnswer: Int = 0
     var allQuestions = QuestionBank()
     
     @IBOutlet weak var questionNumberLabel: UILabel!
@@ -33,10 +34,16 @@ class QuestionController: UIViewController {
     @IBAction func answerTapped(_ sender: AnyObject) {
         
         if sender.tag == 1 {
+            chosenAnswer = 1
+            checkAnswer(sender: sender as! UIButton)
             
         } else if sender.tag == 2 {
+            chosenAnswer = 2
+            checkAnswer(sender: sender as! UIButton)
             
         } else if sender.tag == 3 {
+            chosenAnswer = 3
+            checkAnswer(sender: sender as! UIButton)
             
         }
     }
@@ -47,6 +54,9 @@ class QuestionController: UIViewController {
         
         questionNumberLabel.text = "\(questionNumber+1)/\(allQuestions.questions.count)"
         scoreLabel.text = "\(score)"
+        answerOneButton.backgroundColor = UIColor.gray
+        answerTwoButton.backgroundColor = UIColor.gray
+        answerThreeButton.backgroundColor = UIColor.gray
     }
     
     func nextQuestion() {
@@ -56,6 +66,25 @@ class QuestionController: UIViewController {
         answerOneButton.setTitle("\(question.answerOne)", for: .normal)
         answerTwoButton.setTitle("\(question.answerTwo)", for: .normal)
         answerThreeButton.setTitle("\(question.answerThree)", for: .normal)
+        
     
+    }
+    
+    func checkAnswer(sender: UIButton) {
+        let question = allQuestions.questions[questionNumber]
+        
+        if chosenAnswer == question.questionAnswer {
+          sender.backgroundColor = UIColor.green
+        } else if chosenAnswer != question.questionAnswer {
+            sender.backgroundColor = UIColor.red
+            if question.questionAnswer == 1 {
+                answerOneButton.backgroundColor = UIColor.green
+            } else if question.questionAnswer == 2 {
+                answerTwoButton.backgroundColor = UIColor.green
+            } else if question.questionAnswer == 3 {
+                answerThreeButton.backgroundColor = UIColor.green
+            }
+        }
+        //questionNumber += 1
     }
 }
