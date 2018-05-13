a46416fe4ce7fd315e44b720ae689e81e9413529
peironggg <wupeirong294@gmail.com>
2018-05-13 19:45:56 +0800

    Used CoreData to parse and preload questions

--- RTT/AppDelegate.swift
+++ RTT/AppDelegate.swift
@@ -7,40 +7,157 @@
 //
 
 import UIKit
+import CoreData
+import CSVParser
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+//    func parseCSV (contentsOfURL: URL, encoding: String.Encoding, error: NSErrorPointer) -> [(questionTitle:String, correctAnswer:String, answerOne:String, answerTwo:String, answerThree:String, wrongOrRight:String)]? {
+//        // Load the CSV file and parse it
+//        let delimiter = ","
+//        var items:[(questionTitle:String, correctAnswer:String, answerOne:String, answerTwo:String, answerThree:String, wrongOrRight:String)]?
+//
+//        let content = String(describing: parseCSV(contentsOfURL: contentsOfURL, encoding: encoding, error: error))
+//        items = []
+//        let lines:[String] = content.components(separatedBy: NSCharacterSet.newlines) as [String]
+//
+//        for line in lines {
+//            var values:[String] = []
+//            if line != "" {
+//                // For a line with double quotes
+//                // we use NSScanner to perform the parsing
+//                if line.range(of: "\"") != nil {
+//                    var textToScan:String = line
+//                    var value:NSString?
+//                    var textScanner:Scanner = Scanner(string: textToScan)
+//                    while textScanner.string != "" {
+//
+//                        if (textScanner.string as NSString).substring(to: 1) == "\"" {
+//                            textScanner.scanLocation += 1
+//                            textScanner.scanUpTo("\"", into: &value)
+//                            textScanner.scanLocation += 1
+//                        } else {
+//                            textScanner.scanUpTo(delimiter, into: &value)
+//                        }
+//
+//                        // Store the value into the values array
+//                        values.append(value! as String)
+//
+//                        // Retrieve the unscanned remainder of the string
+//                        if textScanner.scanLocation < textScanner.string.count {
+//                            textToScan = (textScanner.string as NSString).substring(from: textScanner.scanLocation + 1)
+//                        } else {
+//                            textToScan = ""
+//                        }
+//                        textScanner = Scanner(string: textToScan)
+//                    }
+//
+//                    // For a line without double quotes, we can simply separate the string
+//                    // by using the delimiter (e.g. comma)
+//                } else  {
+//                    values = line.components(separatedBy:delimiter)
+//                }
+//
+//                // Put the values into the tuple and add it to the items array
+//                let item = (questionTitle: values[0], questionAnswer: values[1], answerOne: values[2], answerTwo: values[3], answerThree: values[4], wrongOrRight: values[5])
+//                items?.append(item as! (questionTitle: String, correctAnswer: String, answerOne: String, answerTwo: String, answerThree: String, wrongOrRight: String))
+//            }
+//
+//        }
+//
+//        return items
+//    }
+    
+    
+   
+    
+    func removeData () {
+        // Remove the existing items
+            let context = self.persistentContainer.viewContext
+            let fetchRequest: NSFetchRequest<Questions> = Questions.fetchRequest()
+        do {
+        let items = try context.fetch(fetchRequest as! NSFetchRequest<NSFetchRequestResult>) as! [Questions]
+            for item in items {
+                context.delete(item)
+            }
+        } catch {
+            print("Unable to fetch request")
+        }
 
+
+    }
+
+    func preloadData () {
+        // Retrieve data from the source file
+        if let path = Bundle.main.path(forResource: "rttquestions", ofType: "csv") {
+
+            // Remove all the menu items before preloading
+            removeData()
+            do {
+                let csv = try CSVParser(filePath: path)
+                for item in csv {
+                    print(item)
+                    let question = Questions(context: persistentContainer.viewContext)
+                    question.questionTitle = item[0]
+                    question.correctAnswer = item[1]
+                    question.answerOne = item[2]
+                    question.answerTwo = item[3]
+                    question.answerThree = item[4]
+                    question.wrongOrRight = item[5]
+                    try persistentContainer.viewContext.save()
+                }
+            } catch {
+                print("Unable to parse file, \(error)")
+            }
+            
+            
+            
+        }
+    }
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+
+//        let defaults = UserDefaults.standard
+//        let isPreloaded = defaults.bool(forKey: "isPreloaded")
+//        if !isPreloaded {
+//            preloadData()
+//            defaults.set(true, forKey: "isPreloaded")
+//        }
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    // MARK: - Core Data stack
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    // MARK: - Core Data Saving support
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
 
 }
 
--- RTT/Controller/WelcomeController.swift
+++ RTT/Controller/WelcomeController.swift
@@ -14,6 +14,7 @@ class WelcomeController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         beginButton.layer.cornerRadius = 10
         
     }
