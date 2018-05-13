2aebce01a90a74c0e9823dff4d200b87e99e3476
peironggg <wupeirong294@gmail.com>
2018-05-13 21:29:57 +0800

    Settled CoreData for tableviewcell but not yet for collection cells

--- RTT/View/TestScore.swift
+++ RTT/View/TestScore.swift
@@ -7,38 +7,43 @@
 //
 
 import UIKit
+import CoreData
 
 class TestScore: UIViewController, UITableViewDataSource, UITableViewDelegate {
    
     
     @IBOutlet weak var resultsTableView: UITableView!
-    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var score: Int = 0
+    var questionBank:[Questions] = []
     let defaults = UserDefaults.standard
-    let questions = QuestionBank().questions
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         resultsTableView.delegate = self
         resultsTableView.dataSource = self
+        score = defaults.integer(forKey: "score")
         navigationItem.title = "Score: \(score)/3"
         resultsTableView.register(UINib(nibName: "customResultCell", bundle: nil), forCellReuseIdentifier: "resultCell")
+        loadQuestions()
+        configureTableView()
     }
     
 
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return questions.count
+       return questionBank.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = resultsTableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath) as! ResultCell
-        cell.qLabel.text = questions[indexPath.row].questionTitle
-        cell.qNumber.text = "\(indexPath.row + 1)"
-        let pages = defaults.object(forKey: "question\(indexPath.row + 1)") as! Question
-        let wrongRight = pages.wrongOrRight
+        let question = questionBank[indexPath.row]
+        cell.qLabel.text = question.questionTitle
+        cell.qNumber.text = String(indexPath.row + 1)
+        let wrongRight = question.wrongOrRight
         
-        if wrongRight == true {
+        if wrongRight == "true" {
             cell.backgroundColor = UIColor.green
         } else {
             cell.backgroundColor = UIColor.red
@@ -46,5 +51,16 @@ class TestScore: UIViewController, UITableViewDataSource, UITableViewDelegate {
         
         return cell
     }
-    
+    func loadQuestions() {
+        let request: NSFetchRequest<Questions> = Questions.fetchRequest()
+        do {
+            questionBank = try context.fetch(request)
+        } catch {
+            print("Unable to fetch request, \(error)")
+        }
+    }
+    func configureTableView() {
+        resultsTableView.rowHeight = UITableViewAutomaticDimension
+        resultsTableView.estimatedRowHeight = 120
+    }
 }
