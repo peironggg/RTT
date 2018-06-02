744ea0334627b4befaaa941cd8395eb2c5271d09
peironggg <wupeirong294@gmail.com>
2018-06-02 20:33:05 +0800

    Solved button fading out issue

--- RTT/Controller/QuestionController.swift
+++ RTT/Controller/QuestionController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import ChameleonFramework
 
 class QuestionController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout{
 
